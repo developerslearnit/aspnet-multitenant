0713d15b4103cff5d530edec880183283f5f5672
Mark <developerslearnit@gmail.com>
2024-08-08 08:57:19 +0100

    Initial setup

---
+++ ASPMultiTenant.API/Entities/Customer.cs
@@ -0,0 +1,19 @@
+﻿namespace ASPMultiTenant.API.Entities;
+
+public partial class Customer
+{
+    private Customer() { }
+    public Guid CustomerId { get; private set; } = Guid.NewGuid();
+    public string FirstName { get; private set; } = default!;
+    public string LastName { get; private set; } = default!;
+    public string Email { get; private set; } = default!;
+
+    public Customer(Guid customerId,string firstname,string lastName,string email)
+    {
+        CustomerId = customerId;
+        FirstName = firstname;
+
+        LastName = lastName;
+        Email = email;
+    }
+}
---
+++ ASPMultiTenant.API/Persistence/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ASPMultiTenant.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPMultiTenant.API.Persistence;
+
+public sealed class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+
+    }
+
+
+    public DbSet<Customer> Customers { get; set; }
+
+}
